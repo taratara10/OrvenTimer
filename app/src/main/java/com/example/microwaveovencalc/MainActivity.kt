d3ed32f515c0a534f1a958d1556687f7eb31d5f5
taratara10 <shuhei.knhs@gmail.com>
2020-12-31 12:32:04 +0900

    Spinner

--- app/src/main/java/com/example/microwaveovencalc/MainActivity.kt
+++ app/src/main/java/com/example/microwaveovencalc/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,20 +21,36 @@ class MainActivity : AppCompatActivity() {
         packageSec.maxValue = 60
 
 
-
         //Package Spinner
         val packageSpinner = findViewById<Spinner>(R.id.packageSpinner)
         val packageItem = resources.getStringArray(R.array.package_watts_items)
 
-        setSpinnerItem(this,packageSpinner,packageItem)
+        setSpinnerItem(this, packageSpinner, packageItem)
 
         //SelfWattsSpinner
         val selfWattsSpinner = findViewById<Spinner>(R.id.selfWattsSpinner)
         val selfWattsItem = resources.getStringArray(R.array.self_watts_items)
 
-        setSpinnerItem(this, selfWattsSpinner,selfWattsItem)
+        setSpinnerItem(this, selfWattsSpinner, selfWattsItem)
+
+        packageSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            //　アイテムが選択された時
+            override fun onItemSelected(parent: AdapterView<*>?,
+                                        view: View?, position: Int, id: Long) {
+                val spinnerParent = parent as Spinner
+                val item = spinnerParent.selectedItem as String
+                // Kotlin Android Extensions
+                val result = findViewById<TextView>(R.id.result)
+                result.text = item
+            }
+
+            //　アイテムが選択されなかった
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //
+            }
 
         }
+    }
 
     private fun setSpinnerItem(context: Context, spinner: Spinner,item:Array<String>): Spinner {
         val adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, item)
@@ -42,6 +58,7 @@ class MainActivity : AppCompatActivity() {
         spinner.adapter = adapter
         return spinner
          }
+
 
     }
 
