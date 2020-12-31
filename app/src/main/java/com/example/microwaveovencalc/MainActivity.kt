1aae3465c8cd400b9e285d8cb01605c8f183fee1
taratara10 <shuhei.knhs@gmail.com>
2020-12-31 11:47:10 +0900

    create View

---
+++ app/src/main/java/com/example/microwaveovencalc/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.microwaveovencalc
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //NumberPicker
+        val packageMin = findViewById<NumberPicker>(R.id.packageMin)
+        packageMin.minValue = 0
+        packageMin.maxValue = 30
+
+        val packageSec = findViewById<NumberPicker>(R.id.packageSec)
+        packageSec.minValue = 0
+        packageSec.maxValue = 60
+
+
+
+        //Package Spinner
+        val packageSpinner = findViewById<Spinner>(R.id.packageSpinner)
+        val packageItem = resources.getStringArray(R.array.package_watts_items)
+
+        setSpinnerItem(this,packageSpinner,packageItem)
+
+        //SelfWattsSpinner
+        val selfWattsSpinner = findViewById<Spinner>(R.id.selfWattsSpinner)
+        val selfWattsItem = resources.getStringArray(R.array.self_watts_items)
+
+        setSpinnerItem(this, selfWattsSpinner,selfWattsItem)
+
+        }
+
+    private fun setSpinnerItem(context: Context, spinner: Spinner,item:Array<String>): Spinner {
+        val adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        return spinner
+         }
+
+    }
+
+
