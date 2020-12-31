6bbc59b21a27fa20ab15d53fae50bfa0f6ee76dd
taratara10 <shuhei.knhs@gmail.com>
2020-12-31 19:40:54 +0900

    complete function

--- app/src/main/java/com/example/microwaveovencalc/MainActivity.kt
+++ app/src/main/java/com/example/microwaveovencalc/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.microwaveovencalc
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +17,17 @@ class MainActivity : AppCompatActivity(){
         //NumberPicker
         val packageMin = findViewById<NumberPicker>(R.id.packageMin)
         packageMin.minValue = 0
-        packageMin.maxValue = 30
+        packageMin.maxValue = 10
+        packageSec.setOnValueChangedListener(){ numberPicker: NumberPicker, i: Int, i1: Int ->
+            calcWatts()
+        }
 
         val packageSec = findViewById<NumberPicker>(R.id.packageSec)
         packageSec.minValue = 0
         packageSec.maxValue = 60
+        packageSec.setOnValueChangedListener(){ numberPicker: NumberPicker, i: Int, i1: Int ->
+            calcWatts()
+        }
 
         //Package Spinner
         val packageSpinner = findViewById<Spinner>(R.id.packageSpinner)
@@ -35,51 +43,38 @@ class MainActivity : AppCompatActivity(){
 
         packageSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             //　アイテムが選択された時
-            override fun onItemSelected(parent: AdapterView<*>?,
-                                        view: View?, position: Int, id: Long) {
-//                val spinnerParent = parent as Spinner
-//                val item = spinnerParent.selectedItem as String
-//                // Kotlin Android Extensions
-//                val result = findViewById<TextView>(R.id.result)
-//                result.text = item
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                calcWatts()
+                }
 
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        }
+            override fun onNothingSelected(parent: AdapterView<*>?) {  TODO("Not yet implemented") }
+                }
 
         selfWattsSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             //　アイテムが選択された時
-            override fun onItemSelected(parent: AdapterView<*>?,
-                                        view: View?, position: Int, id: Long) {
-//                val spinnerParent = parent as Spinner
-//                val item = spinnerParent.selectedItem as String
-//                // Kotlin Android Extensions
-//                val result = result
-//                result.text = item
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                calcWatts()
 
-            }
+                }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        }
+            override fun onNothingSelected(parent: AdapterView<*>?) { TODO("Not yet implemented") }
+              }
 
 
     }
 
-    private fun setSpinnerItem(context: Context, spinner: Spinner,item:Array<String>): Spinner {
+    //ここからfunction
+
+    private fun setSpinnerItem(context: Context, spinner: Spinner, item: Array<String>): Spinner {
         val adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
         return spinner
          }
 
-     fun calcWatts(){
-        val min = packageMin.displayedValues.toString().toInt()
-        val ss = packageSec.displayedValues.toString().toInt()
+     private fun calcWatts(){
+        val min = packageMin.value.toString().toInt()
+        val ss = packageSec.value.toString().toInt()
 
         val selfWatts = selfWattsSpinner.selectedItem.toString() //500W -> 500:Intに変換
         val selfWattsInt = selfWatts.removeSuffix("W").toInt()
