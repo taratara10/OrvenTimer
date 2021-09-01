c785131d8d1937f505f5a51cbfd29caa0598d070
taratara10 <shuhei.knhs@gmail.com>
2021-09-01 13:01:03 +0900

    refactor: viewBinding deployment. Refactor function to testable

--- app/src/main/java/com/example/ovenTimer/MainActivity.kt
+++ app/src/main/java/com/example/ovenTimer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.ovenTimer
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
@@ -12,104 +14,111 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var prefs: SharedPreferences
 
-    val sectionList = arrayOf<String>("00", "10","20", "30", "40", "50")
+
+    private val sectionList = arrayOf("00", "10","20", "30", "40", "50")
+
+    private var mMin = 0
+    private var mSec = 0
+    private var mPackageWatts = 1
+    private var mSelfWatts = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        prefs = getSharedPreferences("lastSelectedWatts", MODE_PRIVATE)
 
+        binding.apply {
 
-//
-//        numberPicker
-//
-        val packageMin = findViewById<NumberPicker>(R.id.packageMin)
-        packageMin.minValue = 0
-        packageMin.maxValue = 10
-        packageMin.setOnValueChangedListener(){ numberPicker: NumberPicker, i: Int, i1: Int ->
-            calcWatts()
-        }
-
-        val packageSec = findViewById<NumberPicker>(R.id.packageSec)
-        packageSec.minValue = 0
-        packageSec.maxValue = sectionList.size -1
-        packageSec.displayedValues = sectionList
-        packageSec.setOnValueChangedListener(){ numberPicker: NumberPicker, i: Int, i1: Int ->
-            calcWatts()
-        }
-
-//
-//sharedPreferences
-//
-        val prefs = getSharedPreferences("lastSelectedWatts", MODE_PRIVATE)
-        val editor = prefs.edit()
-        val packageSelectedWatts = prefs.getInt("packageSelectedWatts", 0)
-        val selfSelectedWatts = prefs.getInt("selfSelectedWatts", 0)
-
-//
-// spinnerの処理
-//
-        //Package Spinner
-        val packageSpinner = findViewById<Spinner>(R.id.packageSpinner)
-        val packageItem = resources.getStringArray(R.array.package_watts_items)
-        setSpinnerItem(this, packageSpinner, packageItem)
-        packageSpinner.setSelection(packageSelectedWatts)
-
-        //SelfWattsSpinner
-        val selfWattsSpinner = findViewById<Spinner>(R.id.selfWattsSpinner)
-        val selfWattsItem = resources.getStringArray(R.array.self_watts_items)
-        setSpinnerItem(this, selfWattsSpinner, selfWattsItem)
-        selfWattsSpinner.setSelection(selfSelectedWatts)
-
-        val spinnerListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                calcWatts()
-                editor.putInt("packageSelectedWatts",packageSpinner.selectedItemPosition)
-                editor.putInt("selfSelectedWatts",selfWattsSpinner.selectedItemPosition)
-                editor.apply()
+            //numberPicker display warm time
+            setupNumberPicker(npPackageMinute, 0, 10,  null)
+            setupNumberPicker(npPackageSecound, 0, 5, sectionList)
+            npPackageMinute.setOnValueChangedListener { _, _, newVal ->
+                mMin = newVal
+                updateWarmingTimeTextView()
             }
-            override fun onNothingSelected(parent: AdapterView<*>?) { TODO("Not yet implemented") }
+            npPackageSecound.setOnValueChangedListener { _, _, newVal ->
+                //秒数は10秒毎のlistで表示してるので、positionのitemを取得
+                mSec = sectionList[newVal].toInt()
+                updateWarmingTimeTextView()
+            }
+
+            //Spinner display Watts
+            setupSpinner(context = this@MainActivity,
+                spinner = spPackageWatts,
+                item = resources.getStringArray(R.array.package_watts_items),
+                lastSelected = prefs.getInt("packageSelectedWatts", 0))
+            setupSpinner(context = this@MainActivity,
+                spinner = spSelfWatts,
+                item = resources.getStringArray(R.array.self_watts_items),
+                lastSelected = prefs.getInt("selfSelectedWatts", 0))
+
+            spPackageWatts.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    mPackageWatts = convertWattsToInt(parent?.getItemAtPosition(position).toString())
+                    updateWarmingTimeTextView()
+                    prefs.edit().putInt("packageSelectedWatts",position).apply()
+                }
+                override fun onNothingSelected(parent: AdapterView<*>?) { }
+            }
+
+            spSelfWatts.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    mSelfWatts = convertWattsToInt(parent?.getItemAtPosition(position).toString())
+                    updateWarmingTimeTextView()
+                    prefs.edit().putInt("selfSelectedWatts", position).apply()
+                    }
+                    override fun onNothingSelected(parent: AdapterView<*>?) { }
+                }
+
         }
 
-        packageSpinner.onItemSelectedListener = spinnerListener
-        selfWattsSpinner.onItemSelectedListener = spinnerListener
-
-    } //onCreate
-
-
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        return super.onCreateView(name, context, attrs)
     }
-    private fun setSpinnerItem(context: Context, spinner: Spinner, item: Array<String>): Spinner {
+
+    //"500W" -> 500
+    private fun convertWattsToInt(item: String):Int = item.removeSuffix("W").toInt()
+
+    private fun setupNumberPicker(view: NumberPicker, min: Int, max:Int, displayedValue: Array<String>?) {
+        view.minValue = min
+        view.maxValue = max
+        if (displayedValue != null) { view.displayedValues = displayedValue }
+    }
+
+    private fun setupSpinner(context: Context, spinner: Spinner, item: Array<String>, lastSelected: Int){
         val adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
-        return spinner
-         }
-
-     private fun calcWatts(){
-        val min = packageMin.value.toString().toInt()
-        val ss = sectionList[packageSec.value].toInt()
-
-        val selfWatts = selfWattsSpinner.selectedItem.toString() //500W -> 500:Intに変換
-        val selfWattsInt = selfWatts.removeSuffix("W").toInt()
-
-        val packageWatts = packageSpinner.selectedItem.toString()
-        val packageWattsInt = packageWatts.removeSuffix("W").toInt()
-
-        val warmTime = min*60 + ss //秒に変換
-        val packageRequireJules = warmTime * packageWattsInt
-        val selfWattsWarmTime = packageRequireJules/selfWattsInt
-
-        val resultText = "${selfWattsWarmTime/60}分${selfWattsWarmTime%60}秒"
-
-        val result = findViewById<TextView>(R.id.tv_result)
-        result.text = resultText
+        spinner.setSelection(lastSelected)
     }
 
+    private fun updateWarmingTimeTextView(){
+        val warmTime = calcWarmingTime(mMin,mSec,mPackageWatts,mSelfWatts)
+        binding.tvResult.text = String.format("${warmTime/60}分${warmTime%60}秒")
+    }
 
+    private fun calcWarmingTime(min: Int, sec: Int, packageWatts: Int, selfWatts: Int): Int {
+        //秒数に変換
+        val warmingSec = min * 60 + sec
+        //熱量 = ワット x 時間
+        val requireJules = warmingSec * packageWatts
+        //熱量を任意のワット数における秒数に変換
+        return requireJules / selfWatts
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
